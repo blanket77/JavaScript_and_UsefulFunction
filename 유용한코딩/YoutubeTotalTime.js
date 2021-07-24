4d7946f9427a71ef1723aef964a0159890a6e288
blanket77 <quantummik@naver.com>
2021-07-24 22:22:40 +0900

    YoutubeTotalTime modify

--- 유용한코딩/YoutubeTotalTime.js
+++ 유용한코딩/YoutubeTotalTime.js
@@ -1,7 +1,6 @@
 //유튜브 재생목록시간
-
 var select = document.querySelector('div.style-scope ytd-playlist-video-list-renderer')
-inner = select.querySelectorAll(':scope .style-scope ytd-thumbnail .style-scope ytd-thumbnail-overlay-time-status-renderer')
+var inner = select.querySelectorAll(':scope .style-scope ytd-thumbnail .style-scope ytd-thumbnail-overlay-time-status-renderer')
 
 var day=0
 var hour=0
@@ -9,11 +8,11 @@ var min=0
 var sec=0
 
 for(var i =0; i<inner.length; i++){
-    var time =inner[i].innerText.split(":")
-    min += time[0]
-    sec += time[1]
+    var time = inner[i].innerText.split(":")
+    min += parseInt(time[0])
+    sec += parseInt(time[1])
 }
-
+p
 min += parseInt(sec/60)
 hour += parseInt(min/60)
 day += parseInt(hour/24)
@@ -23,6 +22,6 @@ min = min%60
 hour = hour%24
 
 if(day == 0)
-    console.log("총 재생기간은 " + hour + "시 " + min + "분 ", sec + "초입니다.")        
+    console.log("총 재생기간은 " + hour + "시 " + min + "분 ", sec + "초입니다.")
 else
     console.log("총 재생기간은 " + day + "일 " + hour + "시 " + min + "분 ", sec + "초입니다.")
